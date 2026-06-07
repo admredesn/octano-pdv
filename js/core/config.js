9cc6ce766101218f1b65e526d65c55db9db8feb8
admredesn <admredesn@gmail.com>
2026-06-07 14:47:43 -0300

    enviar js/core/config.js

--- js/core/config.js
+++ js/core/config.js
@@ -8,7 +8,7 @@ const SUPABASE_URL = "https://gnlbkwvoqnncpszmokuv.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdubGJrd3ZvcW5uY3Bzem1va3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNjQxNDcsImV4cCI6MjA5NTY0MDE0N30.gAPt0FJ1Msk2Jl_pYGEVrGcmlZzwyMJeQE_eanuFSmc";
 
 // Servidor SEFAZ (mesmo do retaguarda) - emite/cancela NFC-e.
-const SEFAZ_URL = "https://octano-sefaz-production.up.railway.app";
+const SEFAZ_URL = "https://octano-sefaz-production-66d4.up.railway.app";
 
 // Cliente Supabase global
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
